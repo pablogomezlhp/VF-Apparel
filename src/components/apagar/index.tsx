cbffb03e928797a9dacf0ee01478c313247d7b6d
pablogomezlhp <pablogomez.l@live.com>
2023-01-27 09:50:46 -0300

    removed unnecessary folder

--- src/components/apagar/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { StyledContainer } from "./styles";
-
-export function Container({children}: unknown) {
-    // return <StyledContainer>{children}</StyledContainer>
-    return <p>jajaja</p>
-}
